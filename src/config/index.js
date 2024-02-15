b8d6765a56969e9232be2c87958d8f558d067d9a
saiharish017 <saiharish017@gmail.com>
2024-02-15 23:11:34 +0530

    middleware(loggedin,authorized)

--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,11 @@
 import dotenv from "dotenv";
 
-dotenv.config()
-const config={
-    PORT: process.env.PORT || 5000,
-    MONGODB_URL: process.env.MONGODB_URL || " mongodb://localhost:27017/myfristapp",
-    JWT_SECRET: process.env.JWT_SECRET,
-    JWT_EXPITY: process.env.JWT_EXPITY
-
-}
-export default config
+dotenv.config();
+const config = {
+  PORT: process.env.PORT || 5000,
+  MONGODB_URL:
+    process.env.MONGODB_URL || " mongodb://localhost:27017/myfristapp",
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPITY: process.env.JWT_EXPITY,
+};
+export default config;
